b4a7ddcf365ab30b4b36c75f7b24da62f2db922a
Dicky-27 <buwonodirga@gmail.com>
2022-04-16 11:42:59 +0700

    feat(MainActivity): Adjust UI modul2

--- app/src/main/java/com/example/modul1/activity/MainActivity.kt
+++ app/src/main/java/com/example/modul1/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.*
 import com.example.modul1.R
 import com.example.modul1.datasource.LocalData
@@ -20,13 +21,17 @@ class MainActivity : AppCompatActivity() {
         val gallery = findViewById<LinearLayout>(R.id.gallery)
         val inflater = LayoutInflater.from(this)
         for (element in LocalData.images) {
+            var isHidden = true
             val view = inflater.inflate(R.layout.item, gallery, false)
             val title = view.findViewById<TextView>(R.id.text)
             val dateText = view.findViewById<TextView>(R.id.date_text)
             val image = view.findViewById<ImageView>(R.id.image)
+            val sizeView = view.findViewById<LinearLayout>(R.id.linearHorizontal)
+            val cetakButton = view.findViewById<Button>(R.id.cetak)
             title.text = element.title
             dateText.text = "Take on "+element.date
             image.setImageResource(element.name)
+            sizeView.setVisible(isHidden)
             view.setOnLongClickListener {
                 Toast.makeText(
                     this,
@@ -35,12 +40,19 @@ class MainActivity : AppCompatActivity() {
                 ).show()
                 true
             }
+            cetakButton.setOnClickListener {
+                isHidden = !isHidden
+                sizeView.setVisible(isHidden)
+            }
             gallery.addView(view)
         }
     }
+
+    private fun View.setVisible(isHidden: Boolean) {
+        visibility = if (isHidden) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
 }
-
-
-
-
-
---
+++ app/src/main/java/com/example/modul1/model/CartItem.kt
@@ -0,0 +1,7 @@
+package com.example.modul1.model
+
+data class CartItem(
+    val title: String,
+    val count: Int,
+    val size: String
+)
